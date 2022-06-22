b2d7f7ff32142d8f189473fb289589365aa53cfb
Anwar Barakat <brkatanwar0@gmail.com>
2022-06-22 13:17:16 +0300

    add the admin dashboard files

---
+++ resources/lang/en/translation.php
@@ -0,0 +1,5 @@
+<?php
+return [
+
+    'website_title'             => 'Multi Vendor Store',
+];
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +22,9 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::group(['prefix' => '/admin/'], function () {
+
+    Route::resource('dashboard', DashboardController::class);
+});
